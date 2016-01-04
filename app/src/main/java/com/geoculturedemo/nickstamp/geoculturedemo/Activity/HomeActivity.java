296377164972aa7876cf0b9df807b3de926a234e
Stampoulis Nikolaos <nickstamp93@gmail.com>
2016-01-04 20:52:04 +0200

    Home Activity base

---
+++ app/src/main/java/com/geoculturedemo/nickstamp/geoculturedemo/Activity/HomeActivity.java
@@ -0,0 +1,39 @@
+package com.geoculturedemo.nickstamp.geoculturedemo.Activity;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.geoculturedemo.nickstamp.geoculturedemo.R;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private View bCurrentLocation, bPickLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        bCurrentLocation = findViewById(R.id.bCurrentLocation);
+        bPickLocation = findViewById(R.id.bPickLocation);
+
+        bCurrentLocation.setOnClickListener(this);
+        bPickLocation.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bCurrentLocation:
+                Snackbar.make(bCurrentLocation , "Current Location" , Snackbar.LENGTH_SHORT).show();
+                break;
+            case R.id.bPickLocation:
+                Snackbar.make(bPickLocation , "Pick Location" , Snackbar.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
