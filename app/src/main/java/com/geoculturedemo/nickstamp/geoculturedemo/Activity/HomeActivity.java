c664fe83b27e646fe054bdec6856bc50e51f5978
Stampoulis Nikolaos <nickstamp93@gmail.com>
2016-01-06 17:19:16 +0200

    HomeActivity UI
    XML for the Home Activity

--- app/src/main/java/com/geoculturedemo/nickstamp/geoculturedemo/Activity/HomeActivity.java
+++ app/src/main/java/com/geoculturedemo/nickstamp/geoculturedemo/Activity/HomeActivity.java
@@ -1,7 +1,6 @@
 package com.geoculturedemo.nickstamp.geoculturedemo.Activity;
 
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -9,18 +8,11 @@ import com.geoculturedemo.nickstamp.geoculturedemo.R;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private View bCurrentLocation, bPickLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        bCurrentLocation = findViewById(R.id.bCurrentLocation);
-        bPickLocation = findViewById(R.id.bPickLocation);
-
-        bCurrentLocation.setOnClickListener(this);
-        bPickLocation.setOnClickListener(this);
 
 
     }
@@ -28,15 +20,6 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-        switch (v.getId()) {
-            case R.id.bCurrentLocation:
-                Snackbar.make(bCurrentLocation, "Explore Local", Snackbar.LENGTH_SHORT).show();
-
-                break;
-            case R.id.bPickLocation:
-                Snackbar.make(bPickLocation, "Custom Explore", Snackbar.LENGTH_SHORT).show();
-                break;
-        }
     }
 
 
