df13c430571abeea171fdfb91fce206cdd827ad9
Stampoulis Nikolaos <nickstamp93@gmail.com>
2016-01-07 18:33:41 +0200

    GPSUtils

--- app/src/main/java/com/geoculturedemo/nickstamp/geoculturedemo/Activity/HomeActivity.java
+++ app/src/main/java/com/geoculturedemo/nickstamp/geoculturedemo/Activity/HomeActivity.java
@@ -3,8 +3,10 @@ package com.geoculturedemo.nickstamp.geoculturedemo.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.geoculturedemo.nickstamp.geoculturedemo.R;
+import com.geoculturedemo.nickstamp.geoculturedemo.Utils.GPSUtils;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -13,6 +15,12 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        GPSUtils gpsUtils = new GPSUtils(this);
+        if (gpsUtils.canGetLocation())
+            Toast.makeText(this, "Long:" + gpsUtils.getLongitude() + " Lat: " + gpsUtils.getLatitude(), Toast.LENGTH_SHORT).show();
+        else
+            gpsUtils.showGPSErrorDialog();
 
 
     }
