ec4d9976e85d66a5b46c4498daf3e6f91bbb96ab
Stampoulis Nikolaos <nickstamp93@gmail.com>
2016-01-08 19:01:54 +0200

    Movie model class

--- app/src/main/java/com/geoculturedemo/nickstamp/geoculturedemo/Model/Movie.java
+++ app/src/main/java/com/geoculturedemo/nickstamp/geoculturedemo/Model/Movie.java
@@ -4,4 +4,110 @@ package com.geoculturedemo.nickstamp.geoculturedemo.Model;
  * Created by nickstamp on 1/6/2016.
  */
 public class Movie {
+
+    private String imgUrl;
+    private String title;
+    private String cast;
+    private String director;
+    private String writer;
+    private String genre;
+    private String rating;
+    private String runtime;
+    private String synopsis;
+    private String year;
+
+    private String url;
+
+    public Movie() {
+
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public String getCast() {
+        return cast;
+    }
+
+    public void setCast(String cast) {
+        this.cast = cast;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
