8b2380d681a31155374bb548b484d51886a4bf33
Stampoulis Nikolaos <nickstamp93@gmail.com>
2016-01-13 21:40:41 +0200

    Song Fragment
    Implemented the functionality for the song fragment, which parses any
    additional information about the selected song.

--- app/src/main/java/com/geoculturedemo/nickstamp/geoculturedemo/Model/Song.java
+++ app/src/main/java/com/geoculturedemo/nickstamp/geoculturedemo/Model/Song.java
@@ -11,38 +11,30 @@ public class Song implements Serializable {
     private String id;
     private String title;
     private String year;
-    private String lyricsWriter;
+    private String lyricsCreator;
     private final String musicCreator;
-    private String composer;
     private String artist;
+    private String lyrics;
     private String url;
 
 
-    public Song(String id, String title, String year, String lyricsWriter, String musicCreator, String artist, String url) {
+    public Song(String id, String title, String year, String lyricsCreator, String musicCreator, String artist, String url) {
 
         this.id = id;
         this.title = title;
         this.year = year;
-        this.lyricsWriter = lyricsWriter;
+        this.lyricsCreator = lyricsCreator;
         this.musicCreator = musicCreator;
         this.artist = artist;
         this.url = url;
     }
 
-    public String getComposer() {
-        return composer;
+    public String getLyricsCreator() {
+        return lyricsCreator;
     }
 
-    public void setComposer(String composer) {
-        this.composer = composer;
-    }
-
-    public String getLyricsWriter() {
-        return lyricsWriter;
-    }
-
-    public void setLyricsWriter(String lyricsWriter) {
-        this.lyricsWriter = lyricsWriter;
+    public void setLyricsCreator(String lyricsCreator) {
+        this.lyricsCreator = lyricsCreator;
     }
 
     public String getYear() {
@@ -73,7 +65,7 @@ public class Song implements Serializable {
     }
 
     public String getUrl() {
-        return url;
+        return "http://www.stixoi.info/" + url;
     }
 
     public void setUrl(String url) {
@@ -86,5 +78,17 @@ public class Song implements Serializable {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getMusicCreator() {
+        return musicCreator;
+    }
+
+    public String getLyrics() {
+        return lyrics;
+    }
+
+    public void setLyrics(String lyrics) {
+        this.lyrics = lyrics;
     }
 }
