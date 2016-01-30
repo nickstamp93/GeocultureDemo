339c2550343d10631bc82438bade1b5cd263acb6
Stampoulis Nikolaos <nickstamp93@gmail.com>
2016-01-30 23:48:34 +0200

    Fragment Structure set up
    Set up the classes so that the async tasks which download the movie-song
    details are in seperate files, and the fragments for the details can be
    used by the favorites (offline use,without details download);

---
+++ app/src/main/java/com/geoculturedemo/nickstamp/geoculturedemo/Callback/OnSongDetailsDownloaded.java
@@ -0,0 +1,8 @@
+package com.geoculturedemo.nickstamp.geoculturedemo.Callback;
+
+/**
+ * Created by nickstamp on 1/30/2016.
+ */
+public interface OnSongDetailsDownloaded {
+    public void onSongDetailsDownload();
+}
---
+++ app/src/main/java/com/geoculturedemo/nickstamp/geoculturedemo/Parser/MovieDetailsParser.java
@@ -0,0 +1,7 @@
+package com.geoculturedemo.nickstamp.geoculturedemo.Parser;
+
+/**
+ * Created by nickstamp on 1/30/2016.
+ */
+public class MovieDetailsParser {
+}
